38d83cdb7d8dff0cd6ce88e5f964e4405dde6dd6
Toby Clarke <teeohbee@gmail.com>
2015-10-24 11:19:47 +0100

    Removed duplicates from search results

--- js/euroHubController.js
+++ js/euroHubController.js
@@ -1,26 +1,27 @@
-euroHubModule.controller('EuroHubController', ['$scope','hubSearchFactory', function($scope, hubSearchFactory) {
+euroHubModule.controller('EuroHubController', ['$scope', 'hubSearchFactory', function($scope, hubSearchFactory) {
 
   $scope.test = "Hello world!"
 
   function searchType() {
     hubSearchFactory.searchType($scope.searchCategory).then(function(res) {
-       $scope.searchList = res;
+      $scope.searchList = res;
     });
   };
 
   $scope.search = function() {
     var searchTerm = $scope.searchTerm;
     $scope.searchResults = [];
-    for ( var i = 0; i < $scope.searchList.length; i++ ) {
+    for (var i = 0; i < $scope.searchList.length; i++) {
       $.each($scope.searchList[i], function(key, value) {
         if ($scope.arrayCheck(value) && $.inArray(searchTerm, value) !== -1) {
-            $scope.searchResults.push($scope.searchList[i]);
-        }
-        else if ($scope.stringCheck(value) && (value.substring(0,searchTerm.length) === searchTerm)) {
+          $scope.searchResults.push($scope.searchList[i]);
+        } else if ($scope.stringCheck(value) && (value.substring(0, searchTerm.length) === searchTerm)) {
           $scope.searchResults.push($scope.searchList[i]);
         }
       })
     }
+    $scope.searchResults = $.unique($scope.searchResults)
+    console.log($scope.searchResults)
   }
 
   $scope.arrayCheck = function(option) {
@@ -35,24 +36,22 @@ euroHubModule.controller('EuroHubController', ['$scope','hubSearchFactory', func
     }
   }
 
-  $scope.$watch("dropdown.value", function(select){
+  $scope.$watch("dropdown.value", function(select) {
     $scope.searchCategory = select;
     searchType();
   })
 
   $scope.rightPane = "assets/images/euro-money.png";
 
-  $scope.openImage = function(location){
+  $scope.openImage = function(location) {
     $scope.rightPane = location;
   }
 
-  $scope.documentClick = function(location, download){
+  $scope.documentClick = function(location, download) {
     if (download === false) {
       $scope.rightPane = window.location.href + location;
       console.log('clicked');
     }
   }
-
-
 
 }]);
